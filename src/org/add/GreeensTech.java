6ea9f7f1c80bcab82fb13b6275a9b67b0d71fcd2
Vasusr91 <srvasu91@gmail.com>
2020-03-17 10:33:58 +0530

    MyNewProject

---
+++ src/org/add/GreeensTech.java
@@ -0,0 +1,23 @@
+package org.add;
+
+public class GreeensTech {
+
+	public void greensOmr() {
+		System.out.println("OMR");
+	}
+	public void greensPorur() {
+		System.out.println("Porur");
+	}
+	public void greensAdyar() {
+		System.out.println("Adyar");
+	}
+	
+	public static void main(String[] args) {
+		GreeensTech gr = new GreeensTech();
+		gr.greensOmr();
+		gr.greensPorur();
+		gr.greensAdyar();
+
+	}
+	
+}
